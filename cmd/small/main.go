15ed24c02750855d324e1650ab63281fa34fe54b
Stefan VanBuren <stefan@vanburen.xyz>
2026-07-15 15:46:04 -0400

    cmd: Replace ffcli with stdlib flag

--- cmd/small/main.go
+++ cmd/small/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -12,30 +11,24 @@ import (
 	"strings"
 	"text/tabwriter"
 
-	"github.com/peterbourgon/ff/v3/ffcli"
-
 	"github.com/stefanvanburen/small/internal/small"
 )
 
 const name = "small"
 
 func main() {
-	if err := run(context.Background(), os.Args, os.Stdout, os.Stdin); err != nil {
+	if err := run(os.Args, os.Stdout, os.Stdin); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, args []string, stdout io.Writer, stdin *os.File) error {
+func run(args []string, stdout io.Writer, stdin *os.File) error {
 	flagSet := flag.NewFlagSet(name, flag.ExitOnError)
 	var transformName = flagSet.String("t", "", "specify transform type")
-
-	root := &ffcli.Command{
-		Name:    name,
-		FlagSet: flagSet,
-		UsageFunc: func(_ *ffcli.Command) string {
-			var usage strings.Builder
-			usage.WriteString(`USAGE
+	flagSet.Usage = func() {
+		var usage strings.Builder
+		usage.WriteString(`USAGE
   small [-t] <text>
 
 FLAGS
@@ -44,60 +37,60 @@ FLAGS
 SUPPORTED TRANSFORMS
 `)
 
-			// sort the list of supported transforms, for stable output
-			supportedTransforms := small.SupportedTransformations()
-			supportedTransformNames := slices.Collect(maps.Keys(supportedTransforms))
-			slices.Sort(supportedTransformNames)
+		// sort the list of supported transforms, for stable output
+		supportedTransforms := small.SupportedTransformations()
+		supportedTransformNames := slices.Collect(maps.Keys(supportedTransforms))
+		slices.Sort(supportedTransformNames)
 
-			tabWriter := tabwriter.NewWriter(&usage, 0, 0, 1, ' ', 0)
-			for _, name := range supportedTransformNames {
-				out := &bytes.Buffer{}
-				small.PerformTransform(supportedTransforms[name], bytes.NewBufferString(name), out)
-				fmt.Fprintf(
-					tabWriter,
-					"  %s\t%s\n",
-					name,
-					out,
-				)
-			}
-			tabWriter.Flush()
+		tabWriter := tabwriter.NewWriter(&usage, 0, 0, 1, ' ', 0)
+		for _, name := range supportedTransformNames {
+			out := &bytes.Buffer{}
+			small.PerformTransform(supportedTransforms[name], bytes.NewBufferString(name), out)
+			fmt.Fprintf(
+				tabWriter,
+				"  %s\t%s\n",
+				name,
+				out,
+			)
+		}
+		tabWriter.Flush()
 
-			return usage.String()
-		},
-		Exec: func(_ context.Context, args []string) error {
-			info, err := stdin.Stat()
-			if err != nil {
-				return fmt.Errorf("can't access stdin: %s", err)
-			}
-
-			transform, err := small.GetTransform(*transformName)
-			if err != nil {
-				return err
-			}
-
-			if (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
-				if len(args) != 1 {
-					return fmt.Errorf("only a single argument is supported")
-				}
-
-				buf := bytes.NewReader([]byte(args[0]))
-				if err := small.PerformTransform(transform, buf, stdout); err != nil {
-					return fmt.Errorf("could not write to stdout: %s", err)
-				}
-
-				// To play nicely with the terminal, write out a newline.
-				stdout.Write([]byte("\n"))
-			} else if info.Size() > 0 {
-				// We're being piped to
-				// command | small ...
-				if err := small.PerformTransform(transform, stdin, stdout); err != nil {
-					return fmt.Errorf("could not write to stdout: %s", err)
-				}
-			}
-
-			return nil
-		},
+		fmt.Fprint(os.Stderr, usage.String())
 	}
 
-	return root.ParseAndRun(ctx, args[1:])
+	if err := flagSet.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	info, err := stdin.Stat()
+	if err != nil {
+		return fmt.Errorf("can't access stdin: %s", err)
+	}
+
+	transform, err := small.GetTransform(*transformName)
+	if err != nil {
+		return err
+	}
+
+	if (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
+		if len(flagSet.Args()) != 1 {
+			return fmt.Errorf("only a single argument is supported")
+		}
+
+		buf := bytes.NewReader([]byte(flagSet.Args()[0]))
+		if err := small.PerformTransform(transform, buf, stdout); err != nil {
+			return fmt.Errorf("could not write to stdout: %s", err)
+		}
+
+		// To play nicely with the terminal, write out a newline.
+		stdout.Write([]byte("\n"))
+	} else if info.Size() > 0 {
+		// We're being piped to
+		// command | small ...
+		if err := small.PerformTransform(transform, stdin, stdout); err != nil {
+			return fmt.Errorf("could not write to stdout: %s", err)
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/stefanvanburen/small
 
 go 1.25.0
-
-require github.com/peterbourgon/ff/v3 v3.4.0
